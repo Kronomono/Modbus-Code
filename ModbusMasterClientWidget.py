a36f44adab61b64b60c506deaa864c4becdeb6e9
Phakeo Arounerangsy <kronomon1@uri.edu>
2023-06-07 11:42:54 -0400

    Added byte support

--- ModbusMasterClientWidget.py
+++ ModbusMasterClientWidget.py
@@ -25,7 +25,8 @@ class ModbusMasterClientWidget:
 
         # Create a second frame to hold the table and the scrollbar
         self.frame = tk.Frame(self.main_frame)
-        self.frame.pack(side='bottom', fill='x', expand=True)
+        #.frame.pack(side='bottom', fill='x', expand=True)
+        self.frame.place(relx=0.5, rely=0.6, anchor='center', relwidth=1, relheight=0.75)
 
         # Create a scrollbar
         scrollbar = tk.Scrollbar(self.frame)
@@ -45,9 +46,8 @@ class ModbusMasterClientWidget:
 
         # Create the data type dropdown
         self.data_type_var = tk.StringVar(self.root)
-        self.data_type_options = ['holding', 'Float 32 bit', 'ASCII 16 bit', 'Epoch 32 bit', 'Binary',
-                                  'Signed Int 16 bit', 'Unsigned Int 16 bit', 'Boolean', 'Byte', 'Signed Int 32 bit',
-                                  'Unsigned Int 32 bit', 'ALL']
+        self.data_type_options = ['holding', 'Float 32 bit', 'ASCII 16 bit', 'Epoch 32 bit', 'Binary','Unsigned Int 16 bit', 'Boolean', 'Byte',
+                                  'Signed Int 32 bit', 'Unsigned Int 32 bit', 'ALL']
 
         self.data_type_var.set(self.data_type_options[0])
         self.data_type_dropdown = tk.OptionMenu(self.root, self.data_type_var, *self.data_type_options)
@@ -284,7 +284,7 @@ class ModbusMasterClientWidget:
 
     def retrieve_data_thread(self):
         # Define the maximum number of requests per second
-        MAX_REQUESTS_PER_SECOND = 30  # Increase this number to increase the polling rate
+        MAX_REQUESTS_PER_SECOND = 15  # Increase this number to increase the polling rate
         # Retrieve data from the Modbus server
         # Create a rate limiter
         rate_limiter = RateLimiter(max_calls=MAX_REQUESTS_PER_SECOND, period=1.0)
@@ -325,46 +325,48 @@ class ModbusMasterClientWidget:
         raw_values = self.raw_values
         self.table.delete(*self.table.get_children())
         selected_type = self.data_type_var.get()  # Define selected_type before using it
+        float_indices = [0, 1, 2, 3, 4, 5, 25, 26, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+                         117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128,
+                         129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140,
+                         141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152,
+                         153, 154, 155, 156, 157, 158, 159, 160, 313, 314, 569, 570]
+
+        ASCII16bit_indices = [205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219,
+                              220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234,
+                              235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249,
+                              250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264,
+                              265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279,
+                              280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294,
+                              295, 296, 297, 298, 299, 300, 301, 302,
+                              303]
+
+        Signed32Int_indices = [6, 7, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
+                               44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62,
+                               63, 64, 65, 66, 67, 68, 69, 70]
+
+        UnsignedInt16bit_indices = [12, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97,
+                                    98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112,
+                                    113, 114, 115, 116, 161, 162, 163, 164, 165, 166,170, 171,
+                                    172, 173, 306, 312, 418, 419, 420, 421, 422, 423, 424, 425, 426, 427, 428, 429,
+                                    430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 443, 444,
+                                    445, 446, 447, 448, 449, 450, 451, 452, 453, 454, 455, 456, 457, 458, 459,
+                                    460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474,
+                                    475, 476, 477, 478, 479, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489,
+                                    490, 491, 492, 493, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504,
+                                    505, 506, 507, 508, 509, 510, 511, 512, 513, 514, 515, 516, 517, 518, 519,
+                                    520, 521, 522, 523, 524, 525, 526, 527, 528, 529, 530, 531, 532, 533, 534,
+                                    535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 549,
+                                    550, 551, 552, 553, 554, 555, 556, 557, 566]
+
+        Unsigned32Int_indices = [315, 316, 558, 559, 560, 561, 562, 563, 564,565]
+        boolean_indices = [175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190,
+                           191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 567,
+                           568]
+        Unsigned8bit_indices = [167,168,169]
+
+        byte_indices = [304, 305, 307, 308, 309, 310, 311]
         if selected_type == "ALL":
-            float_indices = [0, 1, 2, 3, 4, 5, 25, 26, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-                             117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128,
-                             129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140,
-                             141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152,
-                             153, 154, 155, 156, 157, 158, 159, 160, 313, 314, 569, 570]
 
-            ASCII16bit_indices = [205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219,
-                                  220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234,
-                                  235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249,
-                                  250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264,
-                                  265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279,
-                                  280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294,
-                                  295, 296, 297, 298, 299, 300, 301, 302,
-                                  303]
-
-            Signed32Int_indices = [6, 7, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
-                                   44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62,
-                                   63, 64, 65, 66, 67, 68, 69, 70]
-
-            UnsignedInt16bit_indices = [12, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97,
-                                        98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112,
-                                        113, 114, 115, 116, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171,
-                                        172, 173, 306, 312, 418, 419, 420, 421, 422, 423, 424, 425, 426, 427, 428, 429,
-                                        430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 443, 444,
-                                        445, 446, 447, 448, 449, 450, 451, 452, 453, 454, 455, 456, 457, 458, 459,
-                                        460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474,
-                                        475, 476, 477, 478, 479, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489,
-                                        490, 491, 492, 493, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504,
-                                        505, 506, 507, 508, 509, 510, 511, 512, 513, 514, 515, 516, 517, 518, 519,
-                                        520, 521, 522, 523, 524, 525, 526, 527, 528, 529, 530, 531, 532, 533, 534,
-                                        535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 549,
-                                        550, 551, 552, 553, 554, 555, 556, 557, 566]
-
-            Unsigned32Int_indices = [315, 316, 558, 559, 560, 561, 562, 563, 564,
-                                     565]
-
-            boolean_indices = [175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190,
-                               191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 567,
-                               568]
             # Combine all indices into one list
             all_indices = float_indices + ASCII16bit_indices + Signed32Int_indices + UnsignedInt16bit_indices + Unsigned32Int_indices + boolean_indices
 
@@ -388,6 +390,9 @@ class ModbusMasterClientWidget:
                 elif index in ASCII16bit_indices:
                     translated_value = self.translate_value("ASCII 16 bit", value)
                     data_type = "ASCII 16 bit"
+                elif index in byte_indices:
+                    translated_value = self.translate_value(("Byte"),value)
+                    data_type = "Byte"
                 elif index in Signed32Int_indices:
                     # Check if this is the first index of a pair
                     if index + 1 in Signed32Int_indices and i + 1 < len(all_indices) and all_indices[
@@ -420,12 +425,7 @@ class ModbusMasterClientWidget:
                 self.table.insert('', 'end', values=(self.get_name(index+1), index+1, data_type, translated_value))
 
         elif selected_type == "Float 32 bit":
-            float_indices = [0, 1, 2, 3, 4, 5, 25, 26, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-                             117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128,
-                             129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140,
-                             141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152,
-                             153, 154, 155, 156, 157, 158, 159, 160, 313, 314, 569,
-                             570]  # The indices of the values you want to read as 32-bit floats
+
             for i in range(0, len(float_indices), 2):  # Step by 2
                 index1 = float_indices[i]
                 index2 = float_indices[i + 1] if i + 1 < len(
@@ -437,24 +437,20 @@ class ModbusMasterClientWidget:
                 translated_value =   round(translated_value,3)
                 self.table.insert('', 'end', values=(self.get_name(index1+1),index1+1, "Float 32 bit", translated_value))
         elif selected_type == "ASCII 16 bit":
-            ASCII16bit_indices = [205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219,
-                                  220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234,
-                                  235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249,
-                                  250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264,
-                                  265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279,
-                                  280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294,
-                                  295, 296, 297, 298, 299, 300, 301, 302,
-                                  303]  # The indices of the values you want to read
             for i in range(0, len(ASCII16bit_indices)):
                 index1 = ASCII16bit_indices[i]
                 value1 = raw_values[index1]
                 translated_value = self.translate_value("ASCII 16 bit", value1)
                 # translated_value =   round(translated_value,2)
                 self.table.insert('', 'end', values=(self.get_name(index1+1), index1+1, "ASCII 16 bit", translated_value))
+        elif selected_type == "Byte":
+            for i in range(0, len(byte_indices)):
+                index1 = byte_indices[i]
+                value1 = raw_values[index1]
+                translated_value = self.translate_value("Byte", value1)
+                # translated_value =   round(translated_value,2)
+                self.table.insert('', 'end',values=(self.get_name(index1 + 1), index1 + 1, "Byte", translated_value))
         elif selected_type == "Signed Int 32 bit":
-            Signed32Int_indices = [6, 7, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
-                                   44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62,
-                                   63, 64, 65, 66, 67, 68, 69, 70]  # The indices of the values you want to read
             for i in range(0, len(Signed32Int_indices), 2):  # Step by 2
                 index1 = Signed32Int_indices[i]
                 index2 = Signed32Int_indices[i + 1] if i + 1 < len(
@@ -467,20 +463,6 @@ class ModbusMasterClientWidget:
                 self.table.insert('', 'end',
                                   values=(self.get_name(index1+1), index1+1, "Signed Int 32 bit", translated_value))
         elif selected_type == "Unsigned Int 16 bit":
-            UnsignedInt16bit_indices = [12, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97,
-                                        98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112,
-                                        113, 114, 115, 116, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171,
-                                        172, 173, 306, 312, 418, 419, 420, 421, 422, 423, 424, 425, 426, 427, 428, 429,
-                                        430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 443, 444,
-                                        445, 446, 447, 448, 449, 450, 451, 452, 453, 454, 455, 456, 457, 458, 459,
-                                        460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474,
-                                        475, 476, 477, 478, 479, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489,
-                                        490, 491, 492, 493, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504,
-                                        505, 506, 507, 508, 509, 510, 511, 512, 513, 514, 515, 516, 517, 518, 519,
-                                        520, 521, 522, 523, 524, 525, 526, 527, 528, 529, 530, 531, 532, 533, 534,
-                                        535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 549,
-                                        550, 551, 552, 553, 554, 555, 556, 557,
-                                        566]  # The indices of the values you want to read
             for i in range(0, len(UnsignedInt16bit_indices)):
                 index1 = UnsignedInt16bit_indices[i]
                 value1 = raw_values[index1]
@@ -488,8 +470,6 @@ class ModbusMasterClientWidget:
                 self.table.insert('', 'end',
                                   values=(self.get_name(index1+1), index1+1, "Unsigned Int 16 bit", translated_value))
         elif selected_type == "Unsigned Int 32 bit":
-            Unsigned32Int_indices = [315, 316, 558, 559, 560, 561, 562, 563, 564,
-                                     565]  # The indices of the values you want to read
             for i in range(0, len(Unsigned32Int_indices), 2):  # Step by 2
                 index1 = Unsigned32Int_indices[i]
                 index2 = Unsigned32Int_indices[i + 1] if i + 1 < len(
@@ -502,9 +482,6 @@ class ModbusMasterClientWidget:
                 self.table.insert('', 'end',
                                   values=(self.get_name(index1+1), index1+1, "Unsigned Int 32 bit", translated_value))
         elif selected_type == "Boolean":
-            boolean_indices = [175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190,
-                               191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 567,
-                               568]  # The indices of the values you want to read
             for i in range(0, len(boolean_indices)):
                 index1 = boolean_indices[i]
                 value1 = raw_values[index1]
